39d0b83d70e74ffa6054356121b93fe4cc0e1608
Vasu Verma <vermavasu069@gmail.com>
2024-09-03 12:17:57 +0530

    Dashboard route

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+export default function DashboardLayout({children}: {children: React.ReactNode}){
+    <div>
+        <h1>Dashboard layout</h1>
+        {children}
+    </div>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard(){
+    <div>
+        <h1>Dashboard Page</h1>
+    </div>
+}
